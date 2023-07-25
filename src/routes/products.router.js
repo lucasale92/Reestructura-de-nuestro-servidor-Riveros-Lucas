04517c4d868713d1bbc00be53ac9b61359bf4cf3
Lucas <riveros.lucas07@gmail.com>
2023-07-24 22:53:00 -0300

    desafio

---
+++ src/routes/products.router.js
@@ -0,0 +1,15 @@
+import  express  from "express";
+ export const productsRouter = express.Router();
+import { uploader } from "../utils.js";
+import { productsController } from "../controller/products.controller.js";
+
+//GET = OBTENER TODOS
+prodructsRouter.get("/", productsController.getAll);
+//GET = OBTENER POR ID
+prodructsRouter.get('/:id', productsController.getbyId);
+//POST = CREAR
+prodructsRouter.post('/', uploader.single('thumbnail') ,  productsController.createOne);
+//PUT = MODIFICAR
+prodructsRouter.put('/:id', productsController.updateOne);
+//ELIMINAR
+prodructsRouter.delete('/:id', productsController.deletOne);
